4c688011a3f0f67aec753a4791fa96780833d1d2
umaralam <umaralam29@gmail.com>
2022-10-31 02:26:20 +0530

    tomcat log fetch functionality added

--- log_files.py
+++ log_files.py
@@ -1,6 +1,7 @@
 """
 path finder class
 """
+from cmath import log
 import logging
 from datetime import datetime
 from pathlib import Path
@@ -16,6 +17,7 @@ class LogFileFinder():
         self.input_date = input_date
         self.initializedPath_object = initializedPath_object
         self.is_prism_billing_tlog_path = False
+        self.is_tomcat_billing_tlog_path = False
         # self.is_tomcat_path
     
     def prism_billing_tlog_path(self):
@@ -31,6 +33,20 @@ class LogFileFinder():
         else:
             self.set_prism_billing_path(False)
             logging.error('Prism BILLING tlog path does not exists')
+    
+    def tomcat_billing_tlog_path(self):
+        
+        log_path = self.initializedPath_object
+        
+        tomcat_tlog_path = f"{log_path.tomcat_log_path_dict[log_path.tomcat_tlog_log_path]}/BILLING"
+        path = Path(rf"{tomcat_tlog_path}")
+
+        if path.exists():
+            logging.debug('Tomcat BILLING tlog path exists.')
+            self.set_tomcat_billing_path(True)
+        else:
+            self.set_tomcat_billing_path(False)
+            logging.error('Tomcat BILLING tlog path does not exists')
     
 
     def prism_billing_tlog_files(self, input_trans_date):
@@ -60,6 +76,34 @@ class LogFileFinder():
             logging.exception(error)
         
         return None
+    
+    def tomcat_billing_tlog_files(self, input_trans_date):
+        """
+        function to find tomcat tlog file path
+        """
+        tlog_files = []
+        
+        logPath_object = self.initializedPath_object
+        # log_path.initialize_prism_path()
+
+        tomcat_tlog_path = f"{logPath_object.tomcat_log_path_dict[logPath_object.tomcat_tlog_log_path]}/BILLING"
+        path = Path(rf"{tomcat_tlog_path}")
+
+        try:
+            billing_tlog_files = [p for p in path.glob(f"TLOG_BILLING_{input_trans_date}*.*")]
+            if bool(billing_tlog_files):
+                for prism_billing_files in billing_tlog_files:
+                    tlog_files.append(prism_billing_files)
+                    return tlog_files
+            else:
+                logging.error('Tomcat billing tlog directory does not have %s dated files', input_trans_date)
+
+        except ValueError as error:
+            logging.exception(error)
+        except Exception as error:
+            logging.exception(error)
+        
+        return None
 
     def prism_daemonlog_file(self):
         """
@@ -75,6 +119,20 @@ class LogFileFinder():
             logging.error('Prism daemon log path does not exists')
         return None
     
+    def tomcat_daemonlog_file(self):
+        """
+        function to find tomcat daemon log file path
+        """
+        logPath_object = self.initializedPath_object
+        tomcat_daemon_log_path = f"{logPath_object.tomcat_log_path_dict[logPath_object.tomcat_daemon_log_path]}"
+        
+
+        if tomcat_daemon_log_path:
+            return tomcat_daemon_log_path
+        else:
+            logging.error('Tomcat daemon log path does not exists')
+        return None
+    
     def prism_rootlog_file(self):
         logPath_object = self.initializedPath_object
         prism_root_log_path = f"{logPath_object.prism_log_path_dict[logPath_object.prism_root_log_path]}"
@@ -83,6 +141,16 @@ class LogFileFinder():
             return prism_root_log_path
         else:
             logging.error('Prism root log path does not exists')
+        return None
+    
+    def tomcat_rootlog_file(self):
+        logPath_object = self.initializedPath_object
+        tomcat_root_log_path = f"{logPath_object.tomcat_log_path_dict[logPath_object.tomcat_root_log_path]}"
+
+        if tomcat_root_log_path:
+            return tomcat_root_log_path
+        else:
+            logging.error('Tomcat root log path does not exists')
         return None
 
     def prism_daemonlog_backup_file(self):
@@ -99,6 +167,21 @@ class LogFileFinder():
         else:
             logging.error('Prism daemon backup log path does not exists')
         return None
+    
+    def tomcat_daemonlog_backup_file(self):
+        logPath_object = self.initializedPath_object
+        tomcat_daemon_log_backup = f"{logPath_object.tomcat_log_path_dict[logPath_object.tomcat_daemon_log_backup_path]}"
+        
+        backup_date = self.input_date
+        date = self.get_backup_date(backup_date)
+
+        tomcat_daemon_log_backup_path = f"{tomcat_daemon_log_backup}{date[0]}-{date[1]}/prismD-{date[0]}-{date[1]}-{date[2]}*.gz"
+        
+        if tomcat_daemon_log_backup_path:
+            return tomcat_daemon_log_backup_path
+        else:
+            logging.error('Tomcat daemon backup log path does not exists')
+        return None
 
     def prism_rootlog_backup_file(self):
         logPath_object = self.initializedPath_object
@@ -114,6 +197,21 @@ class LogFileFinder():
         else:
             logging.error('Prism root backup log path does not exists')
         return None
+    
+    def tomcat_rootlog_backup_file(self):
+        logPath_object = self.initializedPath_object
+        tomcat_root_log_backup = f"{logPath_object.tomcat_log_path_dict[logPath_object.tomcat_root_log_backup_path]}"
+        
+        backup_date = self.input_date
+        date = self.get_backup_date(backup_date)
+        
+        tomcat_root_log_backup_path = f"{tomcat_root_log_backup}{date[0]}-{date[1]}/root-{date[0]}-{date[1]}-{date[2]}*.gz"
+
+        if tomcat_root_log_backup_path:
+            return tomcat_root_log_backup_path
+        else:
+            logging.error('Tomcat root backup log path does not exists')
+        return None
 
     def get_backup_date(self, input_date):
         dts = datetime.strptime(input_date, "%Y%m%d")
@@ -122,7 +220,14 @@ class LogFileFinder():
         return date_formated
 
     def set_prism_billing_path(self, is_prism_billing_tlog_path):
+    
         self.is_prism_billing_tlog_path = is_prism_billing_tlog_path
+    
+    def set_tomcat_billing_path(self, is_tomcat_billing_tlog_path):
+        self.is_tomcat_billing_tlog_path = is_tomcat_billing_tlog_path
 
     def get_prism_path(self):
+        return self.is_prism_billing_tlog_path
+    
+    def get_tomcat_path(self):
         return self.is_prism_billing_tlog_path
--- tlog_parser.py
+++ tlog_parser.py
@@ -18,13 +18,13 @@ class TlogParser:
 
     def parse_prism(self):
         """
-        Call to retreive tlog files and parse.
+        Call to retreive prism tlog files and parse.
         """
         is_parsed = False
         tlog_object = Tlog(self.msisdn, self.input_date, self.tlog_record_list, self.initializedPath_object)
     
         if tlog_object.get_prism_billing_tlog():
-            logging.debug('Tlog record found for %s', self.msisdn )
+            logging.debug('Prism tlog record found for %s', self.msisdn )
             filtered_prism_tlog = tlog_object.tlog_record_list
             logging.debug('Parsing tlog file')
             
@@ -40,9 +40,38 @@ class TlogParser:
                     self.dictionary_of_tlogs[f"dict_{cnt}"] = header_data
                 is_parsed = True
             else:
-                logging.error('No tlog found for given msisdn: %s', self.msisdn)
+                logging.error('No prism tlog found for given msisdn: %s', self.msisdn)
         else:
-            logging.error('No tlog found for given msisdn: %s', self.msisdn)
+            logging.error('No prism tlog found for given msisdn: %s', self.msisdn)
+        return is_parsed
+    
+    def parse_tomcat(self):
+        """
+        Call to retreive tomcat tlog files and parse.
+        """
+        is_parsed = False
+        tlog_object = Tlog(self.msisdn, self.input_date, self.tlog_record_list, self.initializedPath_object)
+    
+        if tlog_object.get_tomcat_billing_tlog():
+            logging.debug('Tomcat tlog record found for %s', self.msisdn )
+            filtered_tomcat_tlog = tlog_object.tlog_record_list
+            logging.debug('Parsing tlog file')
+            
+            if filtered_tomcat_tlog:
+                tlog_data = [data.split("|") for data in filtered_tomcat_tlog[0]]
+                for cnt, data in enumerate(tlog_data):
+                    tlog_key_value = {"TIMESTAMP" : "","THREAD" : "","SITE_ID" : "","MSISDN" : "","SUB_TYPE" : "","SBN_ID/EVT_ID" : "","SRV_KEYWORD" : "","CHARGE_TYPE" : "","PARENT_KEYWORD" : "","AMOUNT" : "","MODE" : "","USER" : "","REQUEST_DATE" : "","INVOICE_DATE" : "","EXPIRY_DATE" : "","RETRY_COUNT" : "","CYCLE_STATUS" : "","GRACE_COUNT" : "","GRACE_RETRY_COUNT" : "","NEW_SRV_KEYWORD" : "","INFER_SUB_STATUS" : "","CHARGE_KEYWORD" : "","TRIGGER_ID" : "","PACK_KEY" : "","PARENT_ID" : "","APP_NAME" : "","SITE_NAME" : "","[STCK=NEW_TYPE,MESSAGE]" : "","[CBAL=STATUS,BAL_AMOUNT,CHGMODE,BILLING_REFID,RETCODE,RETMSG,BAL]" : "","[RSRV=STATUS,BAL_AMOUNT,CHGMODE,BILLING_REFID,RETCODE,RETMSG,BAL]" : "","[CHG=PMT_STATUS,BILL_AMOUNT,CHGMODE,BILLING_REFID,RETCODE,RETMSG,RCHG_FILE_CHG,BAL]" : "","[REMT=REMOTE_STATUS,RETCODE,RETMSG]" : "","[CBCK=STATUS,RETCODE,RETMSG]" : "","[CONTENT_ID=[ContentInfo]]" : "","[CAMPAIGN_ID=[campaignId]]" : "","[TOTAL_CHG_AMNT=[totalChgAmnt]]" : "","[RECO:[ReconciliationData]]" : "","[TSK = TASK_TYPE,TASK_STATUS,PAYMENT STATUS,CHARGE_SCHEDULE,NEXT_BILL_DATE]" : ""}
+                    self.dictionary_of_tlogs[f"dict_{cnt}"] = tlog_key_value
+                    header_data = self.dictionary_of_tlogs[f"dict_{cnt}"]
+                    temp = list(header_data)
+                    for counter, tlog_header in enumerate(temp):
+                        header_data[tlog_header] = self.data_in_tlog(data, counter)
+                    self.dictionary_of_tlogs[f"dict_{cnt}"] = header_data
+                is_parsed = True
+            else:
+                logging.error('No tomcat tlog found for given msisdn: %s', self.msisdn)
+        else:
+            logging.error('No tomcat tlog found for given msisdn: %s', self.msisdn)
         return is_parsed
 
     def data_in_tlog(self, data, index):
